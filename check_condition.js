ee776691b9d9fae3e5cac0d13477dbf2ca753d9e
Vivek Singh <105125933+vivek-singh-123@users.noreply.github.com>
2023-03-30 23:21:01 +0530

    Add files via upload

---
+++ check_condition.js
@@ -0,0 +1,17 @@
+let money = 20000
+
+const min_money_for_addmission = 5000
+
+if(money=>min_money_for_addmission ){
+    console.log('🍀')
+}
+
+//another example
+
+let age=35
+
+const min_age_for_vote = 18
+
+if(age=>min_age_for_vote){
+    console.log('You are eligible for vote')
+}
