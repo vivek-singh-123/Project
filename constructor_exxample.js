3c4a9f1b42eb203c36ee1cc4f8e46a5f1c206513
Vivek Singh <105125933+vivek-singh-123@users.noreply.github.com>
2023-04-02 13:17:06 +0530

    Add files via upload

---
+++ constructor_exxample.js
@@ -0,0 +1,13 @@
+class car{
+    constructor(name,model){
+        this.name=name
+        this.model
+    }
+}
+const hiscar=new car('Maserati','A6')
+const yourcar=new car('Audi','A3')
+const mycar=new car('BMW','x7')
+
+console.log(hiscar,yourcar,mycar)
+console.clear()
+console.table[(hiscar,yourcar,mycar)]
