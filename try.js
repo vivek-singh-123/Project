f079382c5946b000a735f2a03efe8111f874dc9d
Vivek Singh <105125933+vivek-singh-123@users.noreply.github.com>
2023-04-02 13:18:37 +0530

    Add files via upload

---
+++ try.js
@@ -0,0 +1,9 @@
+//reduce method
+const numbers=[10,542,8,3556,2135,560]
+const sum=numbers.reduce(function(accumulator,currentvalue){
+    return accumulator  + currentvalue
+})
+console.log(sum)
+
+//map method
+
